e0775eb007ef2a0414fe4aeb3467eeff18f17d0f
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 23:44:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+
+def prime?(integer)
+  integer = ["2", "3", "11", "105557" ]
+  i = 0
+  while i < integer.length 
+    integer[i]
+    i += 1
+  end
+  
+  if integer.prime? (true) 
+    return true 
+  else
+    return false 
+  end
+end
